7991204219322db2b194b7e7eb886fd92b9471a8
Boba-Syu <1914808114@qq.com>
2019-01-16 13:55:40 +0800

    123

--- src/main/java/com/hunau/controller/springbootController.java
+++ src/main/java/com/hunau/controller/springbootController.java
@@ -1,8 +1,11 @@
 package com.hunau.controller;
 
+import com.hunau.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
 
 /**
  * Created by MI on 2019/1/16.
@@ -11,12 +14,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class springbootController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
+    public String index(Map<String, Object> map) {
         return "index";
     }
 
     @RequestMapping(value = "hello", method = RequestMethod.GET)
-    public String hello() {
+    public String hello(Map <String, Object> map) {
+        User user = new User(1, 123);
+        map.put("user", user);
         return "hello";
     }
 }
